5c8dedc932f52110fd862676b2808b513ce35f33
daslu <daniel.slutsky@gmail.com>
2021-01-25 16:55:51 +0200

    config defaults (should have been part of previous commint)

---
+++ src/gorilla_notes/defaults.clj
@@ -0,0 +1,9 @@
+(ns gorilla-notes.defaults)
+
+(def options
+  {:reverse-notes?      true
+   :header?             true
+   :notes-in-cards?     true
+   :initially-collapse? false
+   :auto-scroll?        true
+   :port                1903}) 
