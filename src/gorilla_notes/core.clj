5869a248460cf74afb164bc2d6270d9a0fe29e2c
daslu <daniel.slutsky@gmail.com>
2021-03-13 21:46:06 +0200

    example code

--- src/gorilla_notes/core.clj
+++ src/gorilla_notes/core.clj
@@ -96,6 +96,18 @@
   (add-note!
    [:p/math "(ax ^2 + bx + c = 0 )"])
 
+  (add-note!
+   [:div
+    "."
+    [:p/vega
+     {:description "A simple bar chart with embedded data."
+      :data        {:values [{:a "A" :b 28} {:a "B" :b 55} {:a "C" :b 43}
+                             {:a "D" :b 91} {:a "E" :b 81} {:a "F" :b 53}
+                             {:a "G" :b 19} {:a "H" :b 87} {:a "I" :b 52}]}
+      :mark        :bar
+      :encoding    {:x {:field :a :type :nominal :axis {:labelAngle 0}}
+                    :y {:field :b :type :quantitative}}}]])
+
   (add-note! (into [:div]
                    (repeatedly 9 (fn []
                                    [:p (rand-int 999)]))))
