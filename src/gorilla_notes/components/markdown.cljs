7f0d40f81f2ddd8dd3589301b195419cdca9426c
daslu <daniel.slutsky@gmail.com>
2021-03-14 23:14:30 +0200

    forgot to commit markdown.cljs

---
+++ src/gorilla_notes/components/markdown.cljs
@@ -0,0 +1,26 @@
+(ns gorilla-notes.components.markdown
+  (:require ["react-markdown" :as ReactMarkdown]
+            ["remark-gfm" :as gfm]
+            ["remark-math" :as math]
+            ["react-katex" :refer [InlineMath BlockMath]]
+            ["react" :as React]))
+
+(println [:DBG InlineMath])
+
+(def renderers
+  #js {:inlineMath (fn [v]
+                     (React/createElement InlineMath #js {:math (.-value v)}))
+       :math       (fn [{:keys [value]}]
+                     (React/createElement BlockMath #js {:math value}))})
+
+(defn ^{:category :ui}
+  markdown
+  "reagent markdown render component
+   usage:
+    [markdown markdown-string]"
+  [md]
+  [:div
+   [:> ReactMarkdown
+    {:plugins #js [math gfm]
+     :renderers renderers
+     :children md}]])
